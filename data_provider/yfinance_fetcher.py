492f21763ecde6991c18d28aa39ae9343a8c7b9a
Zhengyi Jiang <anthony.jiang.github@outlook.com>
2026-02-25 21:22:32 +0000

    fixed a problem caused by yfinance using shared DF, which caused error when manipulating data.

--- data_provider/yfinance_fetcher.py
+++ data_provider/yfinance_fetcher.py
@@ -154,13 +154,21 @@ class YfinanceFetcher(BaseFetcher):
         
         try:
             # 使用 yfinance 下载数据
+            
             df = yf.download(
                 tickers=yf_code,
                 start=start_date,
                 end=end_date,
                 progress=False,  # 禁止进度条
                 auto_adjust=True,  # 自动调整价格（复权）
+                multi_level_index=True
             )
+            
+            # 筛选出 yf_code 的列
+            ticker_level = df.columns.get_level_values(1)
+            mask = ticker_level == yf_code
+            if mask.any():
+                df = df.loc[:, mask].copy()
             
             if df.empty:
                 raise DataFetchError(f"Yahoo Finance 未查询到 {stock_code} 的数据")
